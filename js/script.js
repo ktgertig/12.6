27aea16ef3f5ceb33d93eb23202e5ac47054c095
Krzysztof Gertig <ktgertig@gmail.com>
2018-03-17 19:06:31 +0000

    Init project

---
+++ js/script.js
@@ -0,0 +1,26 @@
+$(function() {
+
+	var url = 'https://restcountries.eu/rest/v2/name/';
+	var countriesList = $('#countries');
+
+	$('#search').click(searchCountries);
+
+	function searchCountries() {
+ 		var countryName = $('#country-name').val();
+		if(!countryName.length) countryName = 'Poland';
+			$.ajax({
+  			url: url + countryName,
+  			method: 'GET',
+  			success: showCountriesList
+  		});
+	}
+
+	function showCountriesList(resp) {
+  		countriesList.empty();
+  		resp.forEach(function(item){
+  			$('<img>').attr('src', item.flag).appendTo(countriesList);
+   			$('<li>').text(item.name).appendTo(countriesList);
+		});
+	}
+
+});
